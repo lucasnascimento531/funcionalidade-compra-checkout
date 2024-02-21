7c4f168cab4bd321819e11fe4ffe83ea908d1050
Lucas Cruz <lukkasnascimento2014@gmail.com>
2024-02-21 15:51:17 -0300

    Otimizando Automação

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -13,11 +13,9 @@
     beforeEach('', () => {
         cy.visit('https://practice.automationtesting.in/shop')
     });
-    afterEach('', () => {
-        cy.screenshot()
-    });
+  
 
-    it.only('Deve realizar o check-out e finalizar compra com sucesso', () => {
+    it('Deve realizar o check-out e finalizar compra com sucesso', () => {
         
         cy.get('.post-169').click()
         cy.get('.input-text').clear().type(2)
@@ -36,10 +34,12 @@
         cy.get('#billing_address_1').type(perfil.Endereço)
         cy.get('#billing_city').type(perfil.Cidade)
         cy.get('#select2-chosen-2').type('São Paulo{enter}')
-        cy.get('#billing_postcode').type(perfil.CEP)
+        cy.get('#billing_postcode').type(perfil.cep)
         cy.get('#place_order').click()
 
         cy.get('.woocommerce-thankyou-order-received').should('contain', 'Thank')
+
+        cy.screenshot()
 
     });
 
@@ -56,5 +56,7 @@
 
         cy.get('#place_order').click()
         cy.get('.woocommerce-error').should('contain', 'Billing')
+
+        cy.screenshot()
     });
 });
