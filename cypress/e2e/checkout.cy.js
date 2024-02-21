8857f49082c17a976878b5f9e2689fd1969d4eaa
Lucas Cruz <lukkasnascimento2014@gmail.com>
2024-02-21 06:11:03 -0300

    Automação de testes com cypress

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -17,6 +17,7 @@ context('Funcionalidade: Compra e Checkout', () => {
    });
 
     it('Deve realizar o check-out e finalizar compra com sucesso', () => {
+        
         cy.get('.post-169').click()
         cy.get('.input-text').clear().type(2)
         cy.get('.single_add_to_cart_button').click()
@@ -42,6 +43,7 @@ context('Funcionalidade: Compra e Checkout', () => {
     });
 
     it.only('Deve exibir uma mensagem de alerta para dados inválidos', () => {
+
         cy.get('.post-170').click()
         cy.get('.input-text').clear().type(2)
         cy.get('.single_add_to_cart_button').click()
