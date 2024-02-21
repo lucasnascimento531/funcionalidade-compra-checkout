5279a95967152be92c1ede501b3130a8eb7f25d8
Lucas Cruz <lukkasnascimento2014@gmail.com>
2024-02-21 15:36:12 -0300

    Otimizando automação

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,23 +1,24 @@
 ///<reference types="cypress"/>
 
+    const perfil = require ('../fixtures/perfil.json')
 
 
 
-context('Funcionalidade: Compra e Checkout', () => {
-///Como cliente
-///Quero fazer checkout
-///Para realizar minha compra
+    context('Funcionalidade: Compra e Checkout', () => {
+    ///Como cliente
+    ///Quero fazer checkout
+    ///Para realizar minha compra
 
 
     beforeEach('', () => {
         cy.visit('https://practice.automationtesting.in/shop')
     });
-   afterEach('', () => {
+    afterEach('', () => {
         cy.screenshot()
-   });
+    });
 
-    it('Deve realizar o check-out e finalizar compra com sucesso', () => {
-
+    it.only('Deve realizar o check-out e finalizar compra com sucesso', () => {
+        
         cy.get('.post-169').click()
         cy.get('.input-text').clear().type(2)
         cy.get('.single_add_to_cart_button').click()
@@ -27,18 +28,18 @@ context('Funcionalidade: Compra e Checkout', () => {
         cy.get('.cart_item > .product-name').should('contain', 'Android')
         cy.get('.checkout-button').click()
 
-        cy.get('#billing_first_name').type('Mario')
-        cy.get('#billing_last_name').type('tot')
-        cy.get('#billing_email').type('mariotot22@gmail.com')
-        cy.get('#billing_phone').type('119999999')
-        cy.get('#select2-chosen-1').type("Brazil{enter}")
-        cy.get('#billing_address_1').type('avenida jjjj')
-        cy.get('#billing_city').type('São Paulo')
+        cy.get('#billing_first_name').type(perfil.nome)
+        cy.get('#billing_last_name').type(perfil.sobrenome)
+        cy.get('#billing_email').type(perfil.email)
+        cy.get('#billing_phone').type(perfil.Telefone)
+        cy.get('#select2-chosen-1').type('Brazil{enter}')
+        cy.get('#billing_address_1').type(perfil.Endereço)
+        cy.get('#billing_city').type(perfil.Cidade)
         cy.get('#select2-chosen-2').type('São Paulo{enter}')
-        cy.get('#billing_postcode').type("02225-555")
+        cy.get('#billing_postcode').type(perfil.CEP)
         cy.get('#place_order').click()
 
-        cy.get('.woocommerce-thankyou-order-received').should('contain' , 'Thank')
+        cy.get('.woocommerce-thankyou-order-received').should('contain', 'Thank')
 
     });
 
@@ -54,6 +55,6 @@ context('Funcionalidade: Compra e Checkout', () => {
         cy.get('.checkout-button').click()
 
         cy.get('#place_order').click()
-        cy.get('.woocommerce-error').should('contain' , 'Billing')
+        cy.get('.woocommerce-error').should('contain', 'Billing')
     });
 });
